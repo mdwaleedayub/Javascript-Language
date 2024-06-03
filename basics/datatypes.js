ae009c1cb4702d3b676ae5ead3384019aa3d9953
mdwaleedayub <157424914+mdwaleedayub@users.noreply.github.com>
2024-06-03 18:09:24 +0000

    Datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,40 @@
+//"use strict"; // Defines that JavaScript code should be executed in "strict mode".
+
+/*
+
+The purpose of "use strict" is to indicate that the code should be executed in "strict mode".
+
+With strict mode, you can not, for example, use undeclared variables.
+
+All modern browsers support "use strict" except Internet Explorer 9 
+
+*/
+
+
+// alert("I am Md. Waleed Ayub"); // Cannot be executed because it is running in node and not browser. alert is an inbuilt browser function.
+
+
+console.log(3 + 3); console.log(5); //The code will run however, it is not a proper way of writing code as the readability of this line of code is minimal. Hence, the code we write should be highly readable.
+
+
+/* 
+
+Types of datatypes
+
+
+1) number : 2 to the power of 53
+2) string : ""
+3) bigint : Used for larger values
+4) boolean : true/false
+5) null : special kind of standalone value which represents an empty value
+6) undefined : no value assigned or defined
+7) symbol : used in case of unique components
+
+
+
+typeof function (Gives the datatype of the value): -
+
+console.log(typeof undefined) //Gives undefined as output
+
+console.log(typeof null); // Gives object as output;
+
